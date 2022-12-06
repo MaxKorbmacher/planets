02c4b5f795df661a3d74481c768e451b65892af5
MaxKorbmacher <max.korbmacher@gmail.com>
2022-12-06 16:23:56 +0100

    test commit

---
+++ some_code.R
@@ -0,0 +1,11 @@
+# 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
+usethis::create_github_token()
+
+gitcreds::gitcreds_set()
+
+
+set_github_pat()
+
+credentials::set_github_pat()
+
+usethis::edit_r_environ()
